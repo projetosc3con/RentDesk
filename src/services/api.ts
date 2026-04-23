a7ea0dbfe5122e7a03fe9350fb51bc8619c1223b
projetosc3con <projetosc3con@gmail.com>
2026-04-23 17:10:41 -0400

    minor changes

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { supabase } from '../lib/supabase';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: import.meta.env.VITE_API_ADDRESS,
 });
 
 // Request interceptor to add the JWT token
